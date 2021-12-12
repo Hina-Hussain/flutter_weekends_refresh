95002a997546b5520e289bcb32c8d2b8b220356e
Hina Hussain <engrhina15@outlook.com>
2021-12-12 21:31:41 +0500

    Day 3 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_30_days_challenge/home_page.dart';
+import 'package:flutter_30_days_challenge/pages/home_page.dart';
+import 'package:flutter_30_days_challenge/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      //darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>HomePage()
+      },
     );
   }
 }
